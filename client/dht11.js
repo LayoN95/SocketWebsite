4c3caa4c2d8e5bcc72a43b38160d0c6f593717cf
LayoN95 <mborowczyk95@gmail.com>
2018-11-03 21:26:01 +0100

    Add temp to website

--- client/dht11.js
+++ client/dht11.js
@@ -1,16 +1,21 @@
-var socket = require('socket.io')("http://192.168.1.112:3000"),
+
+var socket = require('socket.io-client')('http://192.168.1.112:3000'),
     ds18b20 = require('ds18b20');
  
 var interval = 3000; //enter the time between sensor queries here (in milliseconds)
  
 //when a client connects
-socket.on('connect', function (socket) {
- 
+
+
+socket.on("connect", function () {
+    socket.on("stateChanged", function(data){
+console.log("update");	
+});	
     var sensorId = [];
     //fetch array containing each ds18b20 sensor's ID
     ds18b20.sensors(function (err, id) {
         sensorId = id;
-        socket.emit('sensors', id); //send sensor ID's to clients
+        socket.emit("sensors", id); //send sensor ID's to clients
         console.log("Sensors" + id);
     });
  
@@ -22,7 +27,7 @@ socket.on('connect', function (socket) {
             ds18b20.temperature(id, function (err, value) {
                 
                 //send temperature reading out to connected clients
-                socket.emit('temps', {'id': id, 'value': value});
+                socket.emit("temps", {"id": id, "value": value});
                 console.log("temperature" + value + " id" + id);
  
             });
