1903c7872e3e325feba299b4ff220c4056a986f7
LayoN95 <mborowczyk95@gmail.com>
2018-11-03 19:07:56 +0100

    update temp

--- client/dht11.js
+++ client/dht11.js
@@ -1,16 +1,17 @@
-var io = require('socket.io').listen(3000),
+var socket = require('socket.io')("http://192.168.1.112:3000"),
     ds18b20 = require('ds18b20');
  
 var interval = 3000; //enter the time between sensor queries here (in milliseconds)
  
 //when a client connects
-io.sockets.on('connection', function (socket) {
+socket.on('connect', function (socket) {
  
     var sensorId = [];
     //fetch array containing each ds18b20 sensor's ID
     ds18b20.sensors(function (err, id) {
         sensorId = id;
         socket.emit('sensors', id); //send sensor ID's to clients
+        console.log("Sensors" + id);
     });
  
     //initiate interval timer
@@ -22,6 +23,7 @@ io.sockets.on('connection', function (socket) {
                 
                 //send temperature reading out to connected clients
                 socket.emit('temps', {'id': id, 'value': value});
+                console.log("temperature" + value + " id" + id);
  
             });
         });
