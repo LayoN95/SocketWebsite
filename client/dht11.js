0b25208380c419e6ab913d51eb0b5d1c8e9cd4a2
LayoN95 <mborowczyk95@gmail.com>
2018-11-05 19:30:57 +0100

    Sensors

--- client/dht11.js
+++ client/dht11.js
@@ -1,37 +1,15 @@
-
 var socket = require('socket.io-client')('http://192.168.1.112:3000'),
-    ds18b20 = require('ds18b20');
- 
-var interval = 3000; //enter the time between sensor queries here (in milliseconds)
- 
-//when a client connects
+	dht11 = require('node-dht-sensor');
 
+dht11.initialize(11,12);
 
-socket.on("connect", function () {
-    socket.on("stateChanged", function(data){
-console.log("update");	
-});	
-    var sensorId = [];
-    //fetch array containing each ds18b20 sensor's ID
-    ds18b20.sensors(function (err, id) {
-        sensorId = id;
-        socket.emit("sensors", id); //send sensor ID's to clients
-        console.log("Sensors" + id);
-    });
- 
-    //initiate interval timer
-    setInterval(function () {
-        //loop through each sensor id
-        sensorId.forEach(function (id) {
- 
-            ds18b20.temperature(id, function (err, value) {
-                
-                //send temperature reading out to connected clients
-                socket.emit("temps", {"id": id, "value": value});
-                console.log("temperature" + value + " id" + id);
- 
-            });
-        });
- 
-    }, interval);
-});
+var interval = setInterval(function () {
+read();
+}, 3000);
+
+function read() {
+	var readout = dht11.read();
+	console.log("Temperatura: " + readout.temperature.toFixed(2) + "C " + "Wilgotnosc: " + readout.humidity.toFixed(2) + "%");
+
+	socket.emit("dht11", {"temperature": readout.temperature.toFixed(2), "humidity": readout.humidity.toFixed(2)});
+};
