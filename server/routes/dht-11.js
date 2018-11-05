b716fa0fb9cf507e12d2291de2a500e470336cc9
LayoN95 <mborowczyk95@gmail.com>
2018-11-05 20:10:11 +0100

    add subpages

--- server/routes/dht-11.js
+++ server/routes/dht-11.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/dht11', function(req, res, next) {
-	res.render('dht-11', {title: 'Express' });
+router.get('/', function(req, res, next) {
+	res.send('dht-11', {title: 'Express' });
 });
 module.export = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-function randomInt (low, high) {
-return Math.floor(Math.random() * (high - low) + low);
-}
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource!')	
