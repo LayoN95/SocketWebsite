3c3dea0d0506a8eb661820a9ec16147ecc9145f7
LayoN95 <mborowczyk95@gmail.com>
2019-01-05 18:46:32 +0100

    pir sensor

---
+++ client/intruderDetecter.js
@@ -0,0 +1,17 @@
+var Gpio = require('onoff').Gpio,
+    pir = new Gpio(27, 'in', 'both');
+var socket = require("socket.io-client")("http://192.168.43.74:3000");
+var count = 0;
+
+exports.start = function () {
+
+
+pir.watch(function(err, value) {
+    count++;
+    console.log('Intruder detected' + count);
+    
+    socket.emit("intruderValue" + value);
+    value = 0;
+});
+    
+    };
