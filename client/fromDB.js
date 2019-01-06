0c378271890826ce91669295f34f3fb8c7833417
LayoN95 <mborowczyk95@gmail.com>
2019-01-06 17:23:17 +0100

    add hcsr04

---
+++ client/fromDB.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "SmartHome",
+	password: "raspberry",
+	database: "SmartHome"
+});
+
+socket.on("readDB", function(){
+     var sql = ("SELECT temperature FROM `DHT11` ORDER BY id DESC LIMIT 10");
+	con.query(sql, function(err, result) {
+		if(err) throw err;
+        socket.emit("dbResult", result);
+        console.log(dbResult);
+}); 
+});
