0857f8b0d390497eb00f313defc83c8a0ff9ac8b
LayoN95 <mborowczyk95@gmail.com>
2019-01-08 19:32:04 +0100

    update

--- client/fromDB.js
+++ client/fromDB.js
@@ -9,16 +9,24 @@ var con = mysql.createConnection({
 	password: "raspberry",
 	database: "SmartHome"
 });
+    
+var dht11_result, light_result;
 
 socket.on("dbRead", function(){
-     var sql = ("SELECT temperature, humidity FROM `DHT11` ORDER BY id DESC LIMIT 1");
-	con.query(sql, function(err, result) {
+     var DHT11 = ("SELECT temperature, humidity FROM `DHT11` ORDER BY id DESC LIMIT 1");
+	 con.query(DHT11, function(err, result) {
 		if(err) throw err;
-        socket.emit("dbResult", result);
-        console.log("Object: " + result[0].humidity);
-        console.log("Object: " + result[0].temperature);
-
-        console.log(result);
+        dht11_result = result; 
+        //socket.emit("dbResult", result);
 }); 
+    var LIGHT = ("SELECT * FROM `LIGHTING`1");
+	con.query(LIGHT, function(err, result) {
+		if(err) throw err;
+        light_result = result;
+        //socket.emit("dbResult", result);
+
+}); 
+    socket.emit("dbResult", {"light": light_result, "dht11": dht11_result});
+    
 });
 };
