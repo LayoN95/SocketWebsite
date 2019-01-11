86baeb44e89a28b0efe1de742bb491fa8af64a41
LayoN95 <mborowczyk95@gmail.com>
2019-01-11 20:18:38 +0100

    update

--- client/setting.js
+++ client/setting.js
@@ -21,7 +21,7 @@ socket.on("time", function(time){
     
     console.log(from, to);
     //var sql = ("INSERT INTO `CONTROL` VALUES ('KITCHEN_LIGHT',"+from+","+to+")");
-	var sql = ("UPDATE `CONTROL` SET FROM = "+from+", TO = "+to+"");
+	var sql = ("UPDATE `CONTROL` SET FROM = '12:00:00', TO = '15:00:00'");
     con.query(sql, function(err, result) {
 		if(err) throw err;
     });
