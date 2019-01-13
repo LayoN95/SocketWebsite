808d6c82e2d3a7780acbf4fa35bf8495137b9413
LayoN95 <mborowczyk95@gmail.com>
2019-01-13 22:19:27 +0100

    update

--- client/fromDB.js
+++ client/fromDB.js
@@ -27,4 +27,13 @@ socket.on("dbRead", function(){
     //socket.emit("dbResult", {"light": light_result, "dht11": dht11_result});
     
 });
+
+//POBRANIE DANYCH Z TABELI CONTROL NA POTRZEBY STEROWANIA URZĄDZENIAMI
+socket.on("getTime", function(){
+	var CONTROL = ("SELECT `DEVICE`, `FROM`, `TO` FROM `CONTROL`");
+	con.query(CONTROL, function(err, result) {
+		if(err) throw err;
+        socket.emit("dbResult", result);
+});
+});
 };
